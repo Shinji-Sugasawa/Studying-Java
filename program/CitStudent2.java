c3d97578e0cf9cd3eca6679cb7f2cb9d377670d5
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-11-14 13:21:05 +0900

    Create CitStudent2.java

---
+++ program/CitStudent2.java
@@ -0,0 +1,18 @@
+package program;
+
+public class CitStudent2 {
+	int no;
+	String name;
+	int year;
+	int grade;
+	int department;
+
+	public void setNo(int p_no) {
+		no = p_no;
+		//no++;
+	}
+	public int getNo() {
+		return no;
+	}
+
+}
