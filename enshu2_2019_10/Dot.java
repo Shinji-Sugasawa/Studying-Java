628992d66efd44ae1cb3a10a7cb5aea42eff055b
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-11-14 13:19:53 +0900

    Create Dot.java

---
+++ enshu2_2019_10/Dot.java
@@ -0,0 +1,13 @@
+package enshu2_2019_10;
+
+import java.awt.Graphics;
+
+class Dot extends Figure {
+	int size ;
+	Dot(){
+		size = 10;
+	}		
+	@Override public void paint(Graphics g) {
+		g.drawOval(x - size/2, y - size/2, size,size);
+	}
+}
