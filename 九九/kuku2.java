75e61fa65ebcf63be290ed6634b6bbaee68dda8d
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-11-14 13:21:11 +0900

    Create kuku2.java

---
+++ 九九/kuku2.java
@@ -0,0 +1,25 @@
+package enshu2_2019_2;
+
+public class kuku2 {
+	public static void main(String args[]) {
+		int y;
+		y = 1;
+		while(y < 10) {
+			
+			sub(y);
+			
+			System.out.println();
+			y++;
+		}
+	}
+	public static void sub(int z) {
+		int x;
+		for(x=1;x<10;x++) {
+			if(x*z<10) {
+				System.out.print(" " +x*z+" ");
+			}else {
+				System.out.print(x*z+" ");
+			}
+		}
+	}
+}
