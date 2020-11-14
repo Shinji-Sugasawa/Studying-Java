8f60d0dd7109b41fa6161e0545b24250d6c00d2f
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-11-14 13:29:28 +0900

    Create Pokemon.java

---
+++ hello/pokemon/Pokemon.java
@@ -0,0 +1,36 @@
+package pokemon;
+
+public class Pokemon {
+
+	private int number;		//番号
+	private String name;	//名前
+	private int attack;		//攻撃力
+	private int defence;	//防御力
+	private int physical;	//体力
+	private String type1;	//タイプ1
+	private String type2;	//タイプ2
+
+	//以下getterとsetter
+
+	public int getNumber() {return number;}
+	public void setNumber(int number) {this.number = number;}
+
+    public String getName() { return name; }
+    public void  setName(String name) { this.name = name; }
+
+	public int  getAttack() { return attack; }
+	public void setAttack(int attack) { this.attack = attack; }
+
+	public int  getDefence() { return defence; }
+	public void setDefence(int defence) { this.defence = defence; }
+
+	public int  getPhysical() { return physical; }
+	public void setPhysical(int physical) { this.physical = physical; }
+
+	public String getType1() { return type1; }
+	public void  setType1(String type1) { this.type1 = type1; }
+
+	public String getType2() { return type2; }
+	public void  setType2(String type2) { this.type2 = type2; }
+
+}
