998e7dff483c70ca7e9f3f1cb640ca9fcb7510c2
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-11-14 13:19:32 +0900

    Create H_28.java

---
+++ 10_30/enshu5_2019_s/H_28.java
@@ -0,0 +1,16 @@
+package enshu5_2019_s;
+
+public class H_28 {
+	int no;
+	String name;
+	int year;
+	int grade;
+	int department ;
+	public void setNo(int p_no) {
+		no = p_no;
+	}
+	public int getNo() {
+		return no;
+	}
+
+}
