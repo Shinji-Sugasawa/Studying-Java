5ec66d272180e1b58b338c7273fa35f02b6d9ed2
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-11-14 13:20:33 +0900

    Create test.java

---
+++ enshu2_2019_7/test.java
@@ -0,0 +1,10 @@
+package enshu2_2019_7;
+
+public class test {
+	public static void main(String args[]) {
+		
+		Circle A = new Circle();
+		System.out.println("x = " + A.x + " y = " +A.y);
+
+	}
+}
